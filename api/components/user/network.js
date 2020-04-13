de27a5296ec54565f0ce7873bb88eaee17fbf5db
luis_parra <luis.parra.pulido@gmail.com>
2020-04-13 12:00:11 -0500

    [ADD] Store - Aislar el código de la base de datos

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-const response = require('../../../network/response')
+const response = require('../../../network/response');
+const Controller = require('./controller');
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  response.success(req, res, 'Everything is working', 200);
+  const list = Controller.list()
+  response.success(req, res, list, 200);
 })
 
 module.exports = router;
